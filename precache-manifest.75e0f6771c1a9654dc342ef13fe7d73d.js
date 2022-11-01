9163fed31da6ecaf9139dca56b450ee9e47967bd
Dominic Paragas <dparagas@berkeley.edu>
2022-10-31 22:14:18 -0700

    Updates

---
+++ precache-manifest.75e0f6771c1a9654dc342ef13fe7d73d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28f1e726fc015204c6ab",
+    "url": "/hw4-dparagas/static/css/main.bed70442.chunk.css"
+  },
+  {
+    "revision": "28f1e726fc015204c6ab",
+    "url": "/hw4-dparagas/static/js/main.28f1e726.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/hw4-dparagas/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "54377ef9c77ae1b88b85",
+    "url": "/hw4-dparagas/static/js/2.54377ef9.chunk.js"
+  },
+  {
+    "revision": "e5c73ba025b0321744ed",
+    "url": "/hw4-dparagas/static/js/runtime~main.e5c73ba0.js"
+  },
+  {
+    "revision": "8ea84bfa10c7562e613a15e9032fb4ef",
+    "url": "/hw4-dparagas/index.html"
+  }
+];
